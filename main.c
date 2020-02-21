b69cad059046cf1601c13f4201f90c43f16f0284
Jorge <jorge.mendes@fatec.sp.gov.br>
2020-02-21 17:03:02 -0300

    Calculadora pronta para entrega

---
+++ main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    char op;
+    int operando1, operando2;
+    int resultado;
+    printf("Qual operação realizar?\n");
+    scanf("%d",&op);
+    printf("Qual o primeiro operando\n");
+    scanf("%d",&operando1);
+    printf("Qual o segundo operando\n");
+    scanf("%d",&operando2);
+    switch(op){
+        case'+':
+        printf("%d + %d = %d\n",operando1,operando2,operando1+operando2);
+        case'-':
+        printf("%d - %d = %d\n",operando1,operando2,operando1-operando2);
+        case'+':
+        printf("%d * %d = %d\n",operando1,operando2,operando1*operando2);
+        case'/':
+        printf("%d / %d = %d\n",operando1,operando2,operando1/operando2);
+        break;
+    }
+    return 0;
+}
